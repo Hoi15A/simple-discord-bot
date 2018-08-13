19dbb671c521bfeec47b5bcef9e7be835d2dd9f0
Nicolas <36765044+waspothegreat@users.noreply.github.com>
2018-08-12 21:31:26 -0600

    Indentation fix

--- commands/invite.js
+++ commands/invite.js
@@ -4,25 +4,25 @@ module.exports = {
       'name': 'invite',
       'permissionLevel': 'everyone',
       'man': '`' + process.env.PREFIX + 'invite`\nDisplays the bot\'s invite'
-      }
-     return info
-    },
-   command: function (msg, params) {
-    var inviteEmbed = {
-    'embed': {
-    'title': 'Invite to the bot',
-    'color': 0xffb6c1,
-    'thumbnail': {
-      'url': message.client.user.avatarURL
-      },
-    'description': `You can invite the bot using [this link](https://discordapp.com/oauth2/authorize?client_id=${message.client.user.id}&scope=bot&permissions=0`,
-    'timestamp': new Date().toDateString(),
-    'footer': {
-      'text': `${message.author.tag}, today at`,
-      'icon_url': message.client.user.avatarURL 
-     }
     }
-   }
-    message.channel.send(inviteEmbed);
+    return info
+  },
+  command: function (msg, params) {
+    var inviteEmbed = {
+      'embed': {
+        'title': 'Invite to the bot',
+        'color': 0xffb6c1,
+        'thumbnail': {
+          'url': msg.client.user.avatarURL
+          },
+        'description': `You can invite the bot using [this link](https://discordapp.com/oauth2/authorize?client_id=${msg.client.user.id}&scope=bot&permissions=0`,
+        'timestamp': new Date().toDateString(),
+            'footer': {
+          'text': `${msg.author.tag}, today at`,
+          'icon_url': msg.client.user.avatarURL 
+         }
+      }
+    }
+    message.channel.send(inviteEmbed)
   }
- }
+}
