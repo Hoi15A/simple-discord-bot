bd560e3300febbdf4d57bad9a254b504fdc71a48
Nicolas <36765044+waspothegreat@users.noreply.github.com>
2018-08-14 22:56:42 -0600

    Potential fix to Issue #12

--- commands/figlet.js
+++ commands/figlet.js
@@ -49,7 +49,7 @@ module.exports = {
       }
       var figText = data.replace(/^(?:[\t ]*(?:\r?\n|\r))+/gm, '')
       if (figText.length < 1990) {
-        msg.channel.send('```\n' + figText + '```')
+        msg.channel.send('```\n' + figText.replace('', 'Empty Output') + '```')
       } else {
         msg.channel.send('Sorry, that message would exceed the max character limit. [' + figText.length + '/2000]')
       }
