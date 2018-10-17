e359ac8da7af8ade054d68652b3c69121521ce37
Austin Albrecht <austin.aa@me.com>
2018-10-17 22:53:31 +0200

    General restructuring of the index file and hopefully a fix for #35

--- index.js
+++ index.js
@@ -20,34 +20,45 @@ if (!fs.existsSync(dir)) {
 var commands = []
 var names = []
 
+childProcess.exec('git rev-parse --short HEAD && git rev-parse HEAD', function (err, stdout) {
+  if (err) {
+    console.log('Unable to fetch commit hash...')
+    console.error(err)
+    return
+  }
+  var lines = stdout.split('\n')
+  process.env.shorthash = lines[0]
+  process.env.hash = lines[1]
+})
+
+var notLoaded = []
+fs.readdirSync('./commands').forEach(file => {
+  if (file.endsWith('.js')) {
+    var module = require('./commands/' + file)
+    if (typeof module.getInfo === 'function' && typeof module.command === 'function') {
+      commands.push({
+        'info': module.getInfo(),
+        'command': module.command,
+        'filename': file
+      })
+    } else {
+      notLoaded.push(file)
+    }
+  }
+})
+
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`)
-  fs.readdirSync('./commands').forEach(file => {
-    commands.push({
-      'info': require('./commands/' + file).getInfo(),
-      'command': require('./commands/' + file).command,
-      'filename': file
-    })
-  })
-
   commands.map(c => {
     if (c.info.enabled) names.push(c.info.name)
   })
-
-  childProcess.exec('git rev-parse --short HEAD && git rev-parse HEAD', function (err, stdout) {
-    if (err) {
-      console.log('Unable to fetch commit hash...')
-      console.error(err)
-      return
-    }
-    var lines = stdout.split('\n')
-    process.env.shorthash = lines[0]
-    process.env.hash = lines[1]
-  })
-
   console.log('━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\n')
   console.log('Following commands have been enabled: ')
   console.log(names.join(', '), '\n')
+  if (notLoaded.length > 0) {
+    console.log('Some commands were not loaded as they are not valid:')
+    console.log(notLoaded.join(', '), '\n')
+  }
 })
 
 client.on('message', msg => {
@@ -78,52 +89,74 @@ client.on('message', msg => {
     }
   })
 
-  if (cmd === 'help' || cmd === 'man') {
-    if (params === 'help' || params === 'man') {
-      msg.reply('you cute little idiot OwO\nYou aleady know how to use ' + cmd + '!')
-      return
-    } else if (params === undefined) {
-      msg.channel.send('The following commands are available:\n```\n' + names.join(', ') +
-      '```\nUse them with: `' + process.env.PREFIX + '<command>`\nAnd use `' + process.env.PREFIX + 'help <command>` to get more information on a specific command.')
-      return
-    }
+  var command
+  switch (cmd) {
+    case 'help':
+    case 'man':
+      if (params === 'help' || params === 'man') {
+        msg.reply('Looks like you already know how to use ' + cmd + '!')
+        return
+      } else if (params === undefined) {
+        msg.channel.send('The following commands are available:\n```\n' + names.join(', ') +
+        '```\nUse them with: `' + process.env.PREFIX + '<command>`\nAnd use `' + process.env.PREFIX + 'help <command>` to get more information on a specific command.')
+        return
+      }
 
-    for (var j = 0; j < commands.length; j++) {
-      if (commands[j].info.name === params) {
-        if (typeof commands[j].info.man === 'string') {
-          msg.channel.send(commands[j].info.man, function (err) {
-            console.log(err)
-          })
-          return
+      for (var j = 0; j < commands.length; j++) {
+        if (commands[j].info.name === params) {
+          if (typeof commands[j].info.man === 'string') {
+            msg.channel.send(commands[j].info.man, function (err) {
+              console.log(err)
+            })
+            return
+          }
         }
       }
-    }
-    msg.channel.send('It seems that there is no help available for: `' + params + '`\n')
-  } else if (cmd === 'commands') {
-    msg.channel.send('The following commands are available:\n```\n' + names.join(', ') + '```')
-  }
+      msg.channel.send('It seems that there is no help available for: `' + params + '`\n')
+      break
 
-  if (permissions.check(msg.member, 'owner')) {
-    var command
-    if (cmd === 'enable') {
+    case 'commands':
+      msg.channel.send('The following commands are available:\n```\n' + names.join(', ') + '```')
+      break
+
+    case 'enable':
+      if (!permissions.check(msg.member, 'owner')) return
       command = commands.find((v) => v.info.name === params)
-      if (command === undefined) return send.standardResponse(msg, 'That command doesn\'t exist!', { name: 'enable command' })
-      if (command.info.enabled) return send.standardResponse(msg, 'That command is already enabled!', { name: 'enable command' })
+      if (command === undefined) {
+        return send.standardResponse(msg, 'That command doesn\'t exist!', { name: 'enable command' })
+      }
+      if (command.info.enabled) {
+        return send.standardResponse(msg, 'That command is already enabled!', { name: 'enable command' })
+      }
       command.info.enabled = true
       names.push(command.info.name)
       names.sort()
       send.standardResponse(msg, 'Successfully enabled the command **' + command.info.name + '**.', { name: 'enabled command' })
-    } else if (cmd === 'disable') {
+      console.log('Enabled:', command.info.name)
+      break
+
+    case 'disable':
+      if (!permissions.check(msg.member, 'owner')) return
       command = commands.find((v) => v.info.name === params)
-      if (command === undefined) return send.standardResponse(msg, 'That command doesn\'t exist!', { name: 'disable command' })
-      if (!command.info.enabled) return send.standardResponse(msg, 'That command is already disabled!', { name: 'disable command' })
+      if (command === undefined) {
+        return send.standardResponse(msg, 'That command doesn\'t exist!', { name: 'disable command' })
+      }
+      if (!command.info.enabled) {
+        return send.standardResponse(msg, 'That command is already disabled!', { name: 'disable command' })
+      }
       command.info.enabled = false
       names.splice(names.indexOf(command.info.name), 1)
       names.sort()
       send.standardResponse(msg, 'Successfully disabled the command **' + command.info.name + '**.', { name: 'disabled command' })
-    } else if (cmd === 'reload') {
+      console.log('Disabled:', command.info.name)
+      break
+
+    case 'reload':
+      if (!permissions.check(msg.member, 'owner')) return
       var commandIndex = commands.findIndex((v) => v.info.name === params)
-      if (commandIndex === -1) return send.standardResponse(msg, 'That command doesn\'t exist!', { name: 'reload' })
+      if (commandIndex === -1) {
+        return send.standardResponse(msg, 'That command doesn\'t exist!', { name: 'reload' })
+      }
       command = commands[commandIndex]
       delete require.cache[require.resolve('./commands/' + command.filename)]
       commands[commandIndex] = {
@@ -134,10 +167,16 @@ client.on('message', msg => {
       if (index !== -1) {
         names.splice(index, 1)
       }
-      if (commands[commandIndex].info.enabled) names.push(commands[commandIndex].info.name)
+      if (commands[commandIndex].info.enabled) {
+        names.push(commands[commandIndex].info.name)
+      }
       names.sort()
       send.standardResponse(msg, 'Successfully reload the command **' + command.info.name + '**.', { name: 'reload' })
-    }
+      console.log('Reloaded:', command.info.name)
+      break
+
+    default:
+      // do nothing
   }
 })
 
